eb4a9ddf8b6f20f46ca6656dfdb67bbea6a55140
allvo6377 <alvinpilot@gmail.com>
2023-06-01 00:39:41 +0300

    hot fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,13 +8,11 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
-
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
 
-	return ((left_height > right_height ? left_height : right_height) + 1);
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
